1c777aa307ca4982ec87a088c075bf430ad0c5e2
hui.shao <hui.shao>
2016-10-31 16:39:02 +0800

    test

--- eloop/eloop.h
+++ eloop/eloop.h
@@ -11,7 +11,7 @@
 // 
 // This software is furnished under a  license  and  may  be used and copied
 // only in accordance with the terms of  such license and with the inclusion
-// of the above copyright notice.  This software or any other copies thereof
+// of the above copyright notice.  This  software or any other copies thereof
 // may not be provided or otherwise made available to  any other person.  No
 // title to and ownership of the software is hereby transferred.
 //
