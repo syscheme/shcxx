aa9db82132b760cf8be5c30356ac482fd974b4ea
codeshawn <1785690193@qq.com>
2016-12-02 11:11:26 +0800

    add dns

---
+++ eloop/eloop_test/Test_DNS/DNS.cpp
@@ -0,0 +1,39 @@
+#include "eloop_net.h"
+#include "eloop.h"
+using namespace ZQ::eloop;
+
+class MyDNS : public DNS
+{
+public:
+	MyDNS() {}
+	~MyDNS() {}
+	virtual void onResolved(Handle::ElpeError status,const char* ip)
+	{
+		if (status != 0) 
+		{
+			fprintf(stderr, "getAddrInfo error %s\n",Error(status).str());
+			return;
+		}
+		printf("ip:%s\n",ip);
+	}
+};
+
+
+int main()
+{
+	Loop loop(true);
+
+	struct addrinfo hints;
+	hints.ai_family = PF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = 0;
+
+	MyDNS dns;
+	dns.getAddrInfo(loop,"reqres.in", "80",&hints);
+
+	loop.run(Loop::Default);
+
+	getchar();
+	return 0;
+}
