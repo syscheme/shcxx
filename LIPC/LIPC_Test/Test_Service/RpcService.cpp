1fdebd623c6db2274836f7f5adadac40ce4544f6
codeshawn <1785690193@qq.com>
2017-08-23 08:52:15 +0800

    update LIPC

--- LIPC/LIPC_Test/Test_Service/RpcService.cpp
+++ LIPC/LIPC_Test/Test_Service/RpcService.cpp
@@ -5,25 +5,27 @@
 class TestRpc
 {
 public:
-	void Print(const ZQ::LIPC::Arbitrary& root, ZQ::LIPC::Arbitrary& response)
+	void Print(const ZQ::LIPC::Arbitrary& root,ZQ::LIPC::PipeConnection& conn)
 	{
+		ZQ::LIPC::Arbitrary response;
 		std::cout << "Receive query: " << root << std::endl;
 		response["jsonrpc"] = "2.0";
 		response["id"] = root["id"];
 		response["result"] = "success";
+		conn.send(response);
 	}
 
-	void Notify(const ZQ::LIPC::Arbitrary& root, ZQ::LIPC::Arbitrary& response)
+	void Notify(const ZQ::LIPC::Arbitrary& root,ZQ::LIPC::PipeConnection& conn)
 	{
 		std::cout << "Notification: " << root << std::endl;
-		response = ZQ::LIPC::Arbitrary::null;
+		//response = ZQ::LIPC::Arbitrary::null;
 	}
 };
 
 int main()
 {
 	ZQ::eloop::Loop loop(false);
-	ZQ::LIPC::JsonRpcService service;
+	ZQ::LIPC::Service service;
 
 	TestRpc test;
 	service.AddMethod(new ZQ::LIPC::RpcMethod<TestRpc>(test, &TestRpc::Print,std::string("print")));
