158e96affdf656833e0c3dd5a1907313ac16a15d
codeshawn <1785690193@qq.com>
2018-01-18 17:22:25 +0800

    add Rtsp test

---
+++ eloop/Http/RtspTest/RtspServer/RtspHandler.cpp
@@ -0,0 +1,42 @@
+#include "RtspHandler.h"
+
+namespace ZQ {
+	namespace eloop {
+
+
+void RTSPTestHandler::onOptions(const RTSPMessage::Ptr& req, RTSPMessage::Ptr& resp)
+{
+
+}
+
+void RTSPTestHandler::onDescribe(const RTSPMessage::Ptr& req, RTSPMessage::Ptr& resp)
+{
+
+}
+
+void RTSPTestHandler::onAnnounce(const RTSPMessage::Ptr& req, RTSPMessage::Ptr& resp)
+{
+
+}
+
+void RTSPTestHandler::onSetup(const RTSPMessage::Ptr& req, RTSPMessage::Ptr& resp)
+{
+
+}
+
+void RTSPTestHandler::onPlay(const RTSPMessage::Ptr& req, RTSPMessage::Ptr& resp)
+{
+
+}
+
+void RTSPTestHandler::onPause(const RTSPMessage::Ptr& req, RTSPMessage::Ptr& resp)
+{
+
+}
+
+void RTSPTestHandler::onTeardown(const RTSPMessage::Ptr& req, RTSPMessage::Ptr& resp)
+{
+
+}
+
+}}
---
+++ eloop/Http/RtspTest/RtspServer/RtspHandler.h
@@ -0,0 +1,35 @@
+#ifndef __RTSP_HANDLER_H__
+#define __RTSP_HANDLER_H__
+
+#include "RTSPServer.h"
+
+namespace ZQ {
+	namespace eloop {
+// ---------------------------------------
+// interface RTSPHandler
+// ---------------------------------------
+class RTSPTestHandler: public RTSPHandler
+{
+public:
+	typedef RTSPApplication<RTSPTestHandler> RTSPTestHandleFactory;
+
+	RTSPTestHandler(IBaseApplication& app, RTSPServer& server, const RTSPHandler::Properties& dirProps = RTSPHandler::Properties())
+		:RTSPHandler(app,server,dirProps)
+	{
+	}
+	~RTSPTestHandler(){}
+
+	virtual void	onOptions(const RTSPMessage::Ptr& req, RTSPMessage::Ptr& resp);
+	virtual void	onDescribe(const RTSPMessage::Ptr& req, RTSPMessage::Ptr& resp);
+
+	virtual void	onAnnounce(const RTSPMessage::Ptr& req, RTSPMessage::Ptr& resp);
+
+	virtual void	onSetup(const RTSPMessage::Ptr& req, RTSPMessage::Ptr& resp);
+	virtual void	onPlay(const RTSPMessage::Ptr& req, RTSPMessage::Ptr& resp);
+	virtual void	onPause(const RTSPMessage::Ptr& req, RTSPMessage::Ptr& resp);
+	virtual void	onTeardown(const RTSPMessage::Ptr& req, RTSPMessage::Ptr& resp);
+
+};
+
+}}
+#endif
