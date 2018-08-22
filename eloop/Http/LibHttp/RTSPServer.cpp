d3070e497d0e6bac4d391b27a3720ae91e349556
codeshawn <1785690193@qq.com>
2018-08-22 16:01:22 +0800

    add rtsp Get_Parameter

--- eloop/Http/LibHttp/RTSPServer.cpp
+++ eloop/Http/LibHttp/RTSPServer.cpp
@@ -326,6 +326,7 @@ void RTSPPassiveConn::OnRequest(RTSPMessage::Ptr req)
 		if (0 == req->method().compare(Method_ANNOUNCE)) { respCode = _rtspHandler->procSessionAnnounce(req, resp, pSess); if (RTSPMessage::Err_AsyncHandling == respCode) return; break; }
 		if (0 == req->method().compare(Method_DESCRIBE)) { respCode = _rtspHandler->procSessionDescribe(req, resp, pSess); if (RTSPMessage::Err_AsyncHandling == respCode) return; break; }
 		if (0 == req->method().compare(Method_TEARDOWN)) { respCode = _rtspHandler->procSessionTeardown(req, resp, pSess); _server.removeSession(pSess->id()); if (RTSPMessage::Err_AsyncHandling == respCode) return; break; }
+		if (0 == req->method().compare(Method_GetParameter)) { respCode = _rtspHandler->procSessionGetParameter(req, resp, pSess); if (RTSPMessage::Err_AsyncHandling == respCode) return; break; }
 
 		respCode =405;
 
