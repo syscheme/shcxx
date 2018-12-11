9470cf60f423557ace5e8659d37902aa5a205381
gitbuild <build.se1@xor-media.tv>
2018-12-11 23:15:03 +0800

    http makefile

--- eloop/Http/LibHttp/HttpServer.h
+++ eloop/Http/LibHttp/HttpServer.h
@@ -112,6 +112,8 @@ protected: // forwarded from HttpConnection
 	HttpHandler::Properties _dirProps;
 };
 
+class HttpPassiveConn;
+
 // ---------------------------------------
 // template HttpApplication
 // ---------------------------------------
